45e62514ae54bf2adc5953c0820f409c8359360f
Neville Muli <neville.muli@gmail.com>
2025-06-01 14:28:12 +0300

    fixes issues with session

--- lib/db/session.py
+++ lib/db/session.py
@@ -2,11 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
-# Create database file if it doesn't exist
-if not os.path.exists("myshop.db"):
-    with open("myshop.db", "w") as f:
-        pass  # Create empty file
-
 # Database URL (matches alembic.ini)
 SQLALCHEMY_DATABASE_URL = "sqlite:///myshop.db"
 
